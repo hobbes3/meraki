2a73f0a66a4247bae04909403cbfb6692b873bdc
Satoshi Kawasaki <hobbes3@gmail.com>
2019-10-29 02:09:39 +0000

    Massive code update for get_data.py. Updated some auto lookups.

--- bin/get_data.py
+++ bin/get_data.py
@@ -11,102 +11,90 @@ import logging
 import logging.handlers
 from random import uniform
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from urllib.parse import urlencode
 from tqdm import tqdm
 from multiprocessing.dummy import Pool
-from multiprocessing import RawValue, Lock
+from multiprocessing import Lock
 from pathlib import Path
 
 from settings import *
 
-# https://stackoverflow.com/a/47562583/1150923
-class Counter(object):
-    def __init__(self, initval=0):
-        self.val = RawValue('i', initval)
-        self.lock = Lock()
+def is_good_meraki_response(r_json, var_type):
+    if "errors" not in r_json and isinstance(r_json, var_type):
+        return True
 
-    def increment(self):
-        with self.lock:
-            self.val.value += 1
+    logger.warning("Bad response: {}".format(r_json))
+    return False
 
-    @property
-    def value(self):
-        return self.val.value
+def get(session, url, params=None, headers=None, auth=None, data=None):
+    return get_post("GET", session, url, params=params, headers=headers, auth=auth, data=data)
 
-def gracefully_exit():
-    with lock:
-        logger.info("INCOMPLETE. Total elapsed seconds: {}.".format(time.time() - start_time))
-        os._exit(1)
+def post(session, url, params=None, headers=None, auth=None, data=None):
+    return get_post("POST", session, url, params=params, headers=headers, auth=auth, data=data)
 
-def log_and_print(msg, log_level="info"):
-    if log_level == "debug":
+def get_post(method, session, url, params=None, headers=None, auth=None, data=None):
+    TRUNCATE = 300
+
+    t0 = time.time()
+    r = None
+
+    full_url = url + "?" + urlencode(params) if params else url
+
+    try:
+        logger.info("Trying to {} {}...".format(method, full_url))
+        if data:
+            msg = "POSTing {} bytes of data".format(len(data))
+            if len(data) <= TRUNCATE:
+                msg += ": {}".format(data)
+                logger.info(msg)
+            else:
+                msg += " (truncated): {}...".format(data[:TRUNCATE])
+                logger.info(msg)
+
+        if method == "GET":
+            r = session.get(url, params=params, headers=headers, auth=auth, data=data)
+        elif method == "POST":
+            r = session.post(url, params=params, headers=headers, auth=auth, data=data)
+        else:
+            logger.error("Unknown method {}!".format(method))
+            return r
+    except Exception:
+        traceback.print_exc()
+        logger.exception("An exception occured!")
+    else:
+        if r.status_code == 200:
+            logger.info("{} {} eventually worked with status code 200.")
+        else:
+            logger.warning("{} {} eventually worked but with status code {}!".format(method, full_url, r.status_code))
+
+        if r.text:
+            if len(r.text) <= TRUNCATE:
+                logger.debug("Response: {}".format(r.text))
+            else:
+                logger.debug("Response (truncated): {}...".format(r.text[:TRUNCATE]))
+        else:
+            logger.warning("Empty response!")
+
+    finally:
+        logger.info("Took {} seconds.".format(time.time() - t0))
+        return r
+
+def log_and_print(msg, level="info"):
+    if level == "debug":
         logger.debug(msg)
+    elif level == "warning":
+        logger.warning(msg)
     else:
         logger.info(msg)
 
     print(msg)
 
-def is_good_meraki_response(response, var_type):
-    if response and "errors" not in response and isinstance(response, var_type):
-        return True
-
-    logger.warning("Bad response: {}".format(response))
-    return False
-
-def get_data(url, headers=None, params=None, give_up=None):
-    return get_post_data(url, method="GET", headers=headers, params=params, give_up=give_up)
-
-def post_data(url, headers=None, params=None, data=None, give_up=None):
-    return get_post_data(url, method="POST", headers=headers, params=params, data=data, give_up=give_up)
-
-def get_post_data(url, method="GET", headers=None, params=None, data=None, give_up=True):
-    count_try = 0
-
-    sleep = uniform(1, 5)
-    logger.debug("Sleeping for {} second(s).".format(sleep))
-    # Sleep first, otherwise too many requests may kick off initially.
-    time.sleep(sleep)
-
-    while True:
-        if count_error.value > TOTAL_ERROR_LIMIT:
-            logger.error("Over {} total errors. Script exiting!".format(count_error.value))
-            gracefully_exit()
-
-        try:
-            if method == "GET":
-                r = requests.get(url, headers=headers, params=params, verify=False, timeout=TIMEOUT)
-            else:
-                r = requests.post(url, headers=headers, params=params, data=data, verify=False, timeout=TIMEOUT)
-                logger.debug("data = {}".format(data))
-
-            logger.debug("{}: {}".format(method, r.url))
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            if str(r.status_code)[:1] == "4":
-                logger.error("Try #{}, total error #{}: {} {} - {}. Skipping!".format(count_try+1, count_error.value, method, url, str(e)))
-                break
-        except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException) as e:
-            if give_up and count_try > len(TRY_SLEEP)-1:
-                logger.error("Try #{}, total error #{}: {} {} - {}. Giving up!".format(count_try+1, count_error.value, method, url, str(e)))
-                break
-
-            count_error.increment()
-            try_sleep = TRY_SLEEP[min(count_try, len(TRY_SLEEP)-1)]
-            logger.error("Try #{}, total error #{}: {} {} - {}. Sleeping for {} second(s).".format(count_try+1, count_error.value, method, url, str(e), try_sleep))
-            time.sleep(try_sleep)
-            count_try += 1
-        except Exception:
-            logger.exception("An exception occured!")
-            traceback.print_exc()
-            gracefully_exit()
-        else:
-            logger.info("Try #{}, total error #{}: {} {} - Success.".format(count_try+1, count_error.value, method, r.url))
-            break
-
-    if method=="GET":
-        if r.text:
-            return r.json()
-        else:
-            logger.warning("Empty response!")
+def gracefully_exit():
+    with lock:
+        logger.info("INCOMPLETE. Total elapsed seconds: {}.".format(time.time() - start_time))
+        os._exit(1)
 
 def update_and_send_devices(device):
     network_id = device["networkId"]
@@ -118,36 +106,42 @@ def update_and_send_devices(device):
     device_status = next((d for d in device_statuses if d["serial"] == device_serial), None)
 
     meraki_url = "https://api.meraki.com/api/v0/networks/{}/devices/{}/uplink".format(network_id, device_serial)
-    uplinks = get_data(meraki_url, headers=meraki_headers, give_up=False)
+    r = get(s, meraki_url, headers=meraki_headers)
 
-    if is_good_meraki_response(uplinks, list):
-        for i, uplink in enumerate(uplinks):
-            # Convert values like "Wan 1" to "wan1"
-            uplinks[i]["interface"] = uplink["interface"].lower().replace(" ", "")
+    if r.text:
+        uplinks = r.json()
 
-        device["uplinks"] = uplinks
+        if is_good_meraki_response(uplinks, list):
+            for i, uplink in enumerate(uplinks):
+                # Convert values like "Wan 1" to "wan1"
+                uplinks[i]["interface"] = uplink["interface"].lower().replace(" ", "")
 
-    if device_status:
-        device["status"] = device_status["status"]
+            device["uplinks"] = uplinks
 
-    if device_model.startswith("MX"):
-        meraki_url = "https://api.meraki.com/api/v0/networks/{}/devices/{}/performance".format(network_id, device_serial)
-        device_perf = get_data(meraki_url, headers=meraki_headers, give_up=False)
+        if device_status:
+            device["status"] = device_status["status"]
 
-        if is_good_meraki_response(device_perf, dict):
-            device.update(device_perf)
+        if device_model.startswith("MX"):
+            meraki_url = "https://api.meraki.com/api/v0/networks/{}/devices/{}/performance".format(network_id, device_serial)
+            rr = get(s, meraki_url, headers=meraki_headers)
 
-    event = {
-        "index": INDEX,
-        "sourcetype": "meraki_api_device",
-        "source": "http:get_data.py",
-        "event": device,
-    }
+            if rr.text:
+                device_perf = rr.json()
 
-    data = json.dumps(event)
+                if is_good_meraki_response(device_perf, dict):
+                    device.update(device_perf)
 
-    logger.info("Sending device {} data to Splunk for network {}.".format(device_serial, network_id))
-    post_data(HTTP_URL, headers=HTTP_HEADERS, data=data, give_up=False)
+        event = {
+            "index": INDEX,
+            "sourcetype": "meraki_api_device",
+            "source": script_path,
+            "event": device,
+        }
+
+        data = json.dumps(event)
+
+        logger.info("Sending device {} data to Splunk for network {}.".format(device_serial, network_id))
+        post(s, HTTP_URL, headers=HTTP_HEADERS, data=data)
 
 def get_and_send_device_loss_and_latency(mx_device):
     network_id = mx_device["networkId"]
@@ -171,28 +165,31 @@ def get_and_send_device_loss_and_latency(mx_device):
     for uplink in ["wan1", "wan2"]:
         params["uplink"] = uplink
 
-        stats = get_data(meraki_url, headers=meraki_headers, params=params, give_up=True)
+        r = get(s, meraki_url, headers=meraki_headers, params=params)
 
-        if is_good_meraki_response(stats, list):
-            device = {
-                "networkId": network_id,
-                "deviceSerial": device_serial,
-                "params": params,
-                "stats": stats,
-            }
+        if r.text:
+            stats = r.json()
 
-            event = {
-                "index": INDEX,
-                "sourcetype": "meraki_api_device_loss_and_latency",
-                "source": "http:get_data.py",
-                "event": device,
-            }
+            if is_good_meraki_response(stats, list):
+                device = {
+                    "networkId": network_id,
+                    "deviceSerial": device_serial,
+                    "params": params,
+                    "stats": stats,
+                }
 
-            data += json.dumps(event)
+                event = {
+                    "index": INDEX,
+                    "sourcetype": "meraki_api_device_loss_and_latency",
+                    "source": script_path,
+                    "event": device,
+                }
+
+                data += json.dumps(event)
 
     if data:
         logger.info("Sending device loss and latency data to Splunk for network {} and device {}.".format(device_serial, network_id))
-        post_data(HTTP_URL, headers=HTTP_HEADERS, data=data, give_up=False)
+        post(s, HTTP_URL, headers=HTTP_HEADERS, data=data)
     else:
         logger.warning("No device loss and latency data to send to Splunk for network {} and device {}.".format(device_serial, network_id))
 
@@ -205,36 +202,40 @@ def get_and_send_clients(device):
         "timespan": REPEAT
     }
 
-    clients = get_data(meraki_url, headers=meraki_headers, params=params)
-    logger.debug("Found {} client(s) for device {}.".format(len(clients), device_serial))
+    r = get(s, meraki_url, headers=meraki_headers, params=params)
 
-    data = ""
+    if r.text:
+        clients = r.json()
+        logger.debug("Found {} client(s) for device {}.".format(len(clients), device_serial))
 
-    if is_good_meraki_response(clients, list):
-        for client in clients:
-            client["networkId"] = network_id
-            client["deviceSerial"] = device_serial
-            client["params"] = params
+        data = ""
 
-            event = {
-                "index": INDEX,
-                "sourcetype": "meraki_api_client",
-                "source": "http:get_data.py",
-                "event": client,
-            }
+        if is_good_meraki_response(clients, list):
+            for client in clients:
+                client["networkId"] = network_id
+                client["deviceSerial"] = device_serial
+                client["params"] = params
 
-            data += json.dumps(event)
+                event = {
+                    "index": INDEX,
+                    "sourcetype": "meraki_api_client",
+                    "source": script_path,
+                    "event": client,
+                }
 
-    if data:
-        logger.info("Sending client data to Splunk for device {}.".format(device_serial))
-        post_data(HTTP_URL, headers=HTTP_HEADERS, data=data, give_up=False)
-    else:
-        logger.warning("No client data to send to Splunk for device {}.".format(device_serial))
+                data += json.dumps(event)
+
+        if data:
+            logger.info("Sending client data to Splunk for device {}.".format(device_serial))
+            post(s, HTTP_URL, headers=HTTP_HEADERS, data=data)
+        else:
+            logger.warning("No client data to send to Splunk for device {}.".format(device_serial))
 
 if __name__ == "__main__":
     start_time = time.time()
 
     setting_file = Path(os.path.dirname(os.path.realpath(__file__)) + "/settings.py")
+    script_path = os.path.realpath(__file__)
 
     if not os.path.exists(setting_file):
         sys.exit("The config file, settings.py, doesn't exist! Please copy, edit, and rename default_settings.py to settings.py.")
@@ -249,9 +250,16 @@ if __name__ == "__main__":
 
     logger.info("===START OF SCRIPT===")
 
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    count_error = Counter(0)
     lock = Lock()
+
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    s = requests.Session()
+    s.verify = False
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504], method_whitelist=frozenset(['GET', 'POST']))
+    adapter = HTTPAdapter(max_retries=retries)
+    s.mount('http://', adapter)
+    s.mount('https://', adapter)
 
     meraki_headers = {
         "X-Cisco-Meraki-API-Key": API_KEY,
@@ -265,42 +273,51 @@ if __name__ == "__main__":
 
         log_and_print("Getting networks...")
         meraki_url = "https://api.meraki.com/api/v0/organizations/{}/networks".format(ORG_ID)
-        networks = get_data(meraki_url, headers=meraki_headers, give_up=False)
-        log_and_print("Found {} networks.".format(len(networks)), log_level="debug")
+        r = get(s, meraki_url, headers=meraki_headers)
 
-        data = ""
+        if r.text:
+            networks = r.json()
+            log_and_print("Found {} networks.".format(len(networks)), level="debug")
 
-        if is_good_meraki_response(networks, list):
-            # Sending data to Splunk via HEC in batch mode.
-            for network in networks:
-                # Improved network tags so they become multivalues in Splunk instead of a space-delimited string
-                network["tags"] = network["tags"].split() if network.get("tags") else None
+            data = ""
 
-                event = {
-                    "index": INDEX,
-                    "sourcetype": "meraki_api_network",
-                    "source": "http:get_data.py",
-                    "event": network,
-                }
+            if is_good_meraki_response(networks, list):
+                # Sending data to Splunk via HEC in batch mode.
+                for network in networks:
+                    # Improved network tags so they become multivalues in Splunk instead of a space-delimited string
+                    network["tags"] = network["tags"].split() if network.get("tags") else None
 
-                data += json.dumps(event)
+                    event = {
+                        "index": INDEX,
+                        "sourcetype": "meraki_api_network",
+                        "source": script_path,
+                        "event": network,
+                    }
 
-            log_and_print("Sending network data to Splunk...")
-            post_data(HTTP_URL, headers=HTTP_HEADERS, data=data, give_up=False)
+                    data += json.dumps(event)
+
+                log_and_print("Sending network data to Splunk...")
+                post(s, HTTP_URL, headers=HTTP_HEADERS, data=data)
 
         # Currently this is the only way to get uplink ip, which is needed for per-device /lossAndLatencyHistory later.
         log_and_print("Getting device statuses...")
+        device_statuses = []
         meraki_url = "https://api.meraki.com/api/v0/organizations/{}/deviceStatuses".format(ORG_ID)
-        device_statuses = get_data(meraki_url, headers=meraki_headers, give_up=False)
-        log_and_print("Found {} device statuses.".format(len(device_statuses)), log_level="debug")
+        r = get(s, meraki_url, headers=meraki_headers)
+        if r.text:
+            device_statuses = r.json()
+            log_and_print("Found {} device statuses.".format(len(device_statuses)), level="debug")
 
         # DEBUG
-        networks = networks[:25]
+        #networks = networks[:20]
 
         log_and_print("Getting devices...")
+        devices = []
         meraki_url = "https://api.meraki.com/api/v0/organizations/{}/devices".format(ORG_ID)
-        devices = get_data(meraki_url, headers=meraki_headers, give_up=False)
-        log_and_print("Found {} devices.".format(len(devices)), log_level="debug")
+        r = get(s, meraki_url, headers=meraki_headers)
+        if r.text:
+            devices = r.json()
+            log_and_print("Found {} devices.".format(len(devices)), level="debug")
 
         log_and_print("Updating and sending devices data...")
         for _ in tqdm(pool.imap_unordered(update_and_send_devices, devices), total=len(devices)):
@@ -309,14 +326,14 @@ if __name__ == "__main__":
         mx_devices = [d for d in devices if d["model"].startswith("MX")]
 
         # DEBUG
-        mx_devices = mx_devices[:25]
+        #mx_devices = mx_devices[:20]
 
         log_and_print("Getting and sending device loss and latency data per device...")
         for _ in tqdm(pool.imap_unordered(get_and_send_device_loss_and_latency, mx_devices), total=len(mx_devices)):
             pass
 
         # DEBUG
-        devices = devices[:50]
+        #devices = devices[:20]
 
         log_and_print("Getting and sending client data per device...")
         for _ in tqdm(pool.imap_unordered(get_and_send_clients, devices), total=len(devices)):
@@ -337,5 +354,4 @@ if __name__ == "__main__":
         pool.join()
         gracefully_exit()
 
-    logger.info("DONE. Total elapsed seconds: {}.".format(time.time() - start_time))
-    print("Done!")
+    log_and_print("DONE. Total elapsed seconds: {}.".format(time.time() - start_time))
